f4b33a2eb1e948a59d48f97c6e237382825b9fed
Guilherme Biegelmeyer <guilherme.biegelmeyer@saipos.com>
2021-07-09 13:18:04 -0300

    - fix update webpack error

--- src/util/WAPI.js
+++ src/util/WAPI.js
@@ -62,7 +62,7 @@ if (!window.Store) {
         const parasite = `parasite${Date.now()}`
         // webpackJsonp([], { [parasite]: (x, y, z) => getStore(z) }, [parasite]);
         if (typeof webpackJsonp === 'function') webpackJsonp([], { [parasite]: (x, y, z) => getStore(z) }, [parasite]);
-        else webpackChunkbuild.push([[parasite], {}, function (o, e, t) { let modules = []; for (let idx in o.m) { modules.push(o(idx)); } getStore(modules); }]);
+        else webpackChunkwhatsapp_web_client.push([[parasite], {}, function (o, e, t) { let modules = []; for (let idx in o.m) { modules.push(o(idx)); } getStore(modules); }]);
     })();
 }
 
